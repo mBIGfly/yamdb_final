0cbebbffd59dfb01a133c1a393f0423a6b475bdf
mBIGfly <84184582+mBIGfly@users.noreply.github.com>
2022-04-17 17:05:28 +0300

    badge

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -133,9 +133,6 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAdmin',
-    # ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
@@ -148,3 +145,6 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
               'AUTH_HEADER_TYPES': ('Bearer',), }
+
+
+AUTH_USER_MODEL = 'users.User'
